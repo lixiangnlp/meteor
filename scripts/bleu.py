d23299c08a3b57185753ab49db64170d60abe463
mjdenkowski <michael.j.denkowski@gmail.com>
2014-03-17 23:09:47 -0400

    Run BLEU and TER for WMT

--- scripts/bleu.py
+++ scripts/bleu.py
@@ -1,53 +1,56 @@
 #!/usr/bin/env python
 
-import codecs, os, shutil, subprocess, sys, tempfile
+import os, shutil, subprocess, sys, tempfile
 
-mteval_pl = os.path.join(os.path.dirname(os.path.dirname(__file__)),
-  'mt-diff', 'files', 'mteval-v13m.pl')
+mteval_pl = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'mt-diff', 'files', 'mteval-v13m.pl'))
 
 def main(argv):
     
     if len(argv[1:]) < 2:
-        print 'Score with NIST BLEU'
-        print ''
-        print 'usage: {0} <hyp> <ref> [opts]'.format(argv[0])
-        print ''
-        print '-------------------'
-        print 'Options for scoring'
-        print '-------------------'
-        print ''
-        subprocess.call(['perl', mteval_pl, '-h'])
+        print >> sys.stderr, 'Usage: {0} <hyp> <ref> [--no-norm] [--char]'.format(argv[0])
+        print >> sys.stderr, 'Segment scores to stderr, final to stdout'
         sys.exit(1)
     
-    hyp = argv[1]
-    ref = argv[2]
+    work = tempfile.mkdtemp(prefix='bleu.')
+    src_sgm = os.path.join(work, 'src.sgm')
+    tst_sgm = os.path.join(work, 'tst.sgm')
+    ref_sgm = os.path.join(work, 'ref.sgm')
+    
     opts = argv[3:]
-    
-    src_sgm = tempfile.mktemp(suffix='.sgm')
-    tst_sgm = tempfile.mktemp(suffix='.sgm')
-    ref_sgm = tempfile.mktemp(suffix='.sgm')
-    
-    sgm(ref, src_sgm, 'srcset')
-    sgm(hyp, tst_sgm, 'tstset')
-    sgm(ref, ref_sgm, 'refset')
 
-    cmd = ['perl', mteval_pl, '-s', src_sgm, '-t', tst_sgm, '-r', ref_sgm]
+    cmd = ['perl', mteval_pl, '-s', src_sgm, '-t', tst_sgm, '-r', ref_sgm, '--metricsMATR']
+    out = open(os.path.join(work, 'out'), 'w')
+    err = open(os.path.join(work, 'err'), 'w')
+    char = False
     for opt in opts:
-        cmd.append(opt)
-    subprocess.call(cmd)
-    
-    os.remove(src_sgm)
-    os.remove(tst_sgm)
-    os.remove(ref_sgm)
+        if opt == '--char':
+            char = True
+        else:
+            cmd.append(opt)
 
-def sgm(f_in, f_out, f_type):
-    i = open(f_in)
+    sgm(argv[2], src_sgm, 'srcset', char)
+    sgm(argv[1], tst_sgm, 'tstset', char)
+    sgm(argv[2], ref_sgm, 'refset', char)
+
+    subprocess.Popen(cmd, stdout=out, stderr=err, cwd=work).wait()
+    
+    for line in open(os.path.join(work, 'BLEU-seg.scr')):
+        print >> sys.stderr, line.split()[-1]
+        
+    print open(os.path.join(work, 'BLEU-sys.scr')).readline().split()[-1]
+
+    shutil.rmtree(work)
+    
+def sgm(f_in, f_out, f_type, char=False):
+    i = open(f_in, 'r')
     o = open(f_out, 'w')
     s = 0
     print >> o, '<{0} trglang="trg" setid="set" srclang="src">'.format(f_type)
     print >> o, '<doc docid="doc" sysid="sys">'
     for line in i:
         s += 1
+        if char:
+            line = ' '.join([ch for ch in line if ch != ' '])
         print >> o, '<seg id="{0}"> {1} </seg>'.format(s, line.strip())
     print >> o, '</doc>'
     print >> o, '</{0}>'.format(f_type)
--- scripts/wmt-eval.sh
+++ scripts/wmt-eval.sh
@@ -18,7 +18,7 @@ for SET in $(ls $PLAIN/system-outputs) ; do
         for HYP in $PLAIN/system-outputs/$SET/$LANG-en/* ; do
             SYS=$(echo $HYP | sed -re "s/.+$SET\.$LANG-en\.//")
             echo " $SYS"
-            java -Xmx700M -jar $METEOR $HYP $PLAIN/references/$SET-ref.en -l en -norm | $FMT $LANG-en $SET $SYS Meteor
+            java -Xmx700M -jar $METEOR $HYP $PLAIN/references/$SET-ref.en -l en -norm -t universal | $FMT $LANG-en $SET $SYS Meteor
         done
     done
     for LANG in cs de es fr ru ; do
@@ -26,7 +26,7 @@ for SET in $(ls $PLAIN/system-outputs) ; do
         for HYP in $PLAIN/system-outputs/$SET/en-$LANG/* ; do
             SYS=$(echo $HYP | sed -re "s/.+$SET\.en-$LANG\.//")
             echo " $SYS"
-            java -Xmx700M -jar $METEOR $HYP $PLAIN/references/$SET-ref.$LANG -l $LANG -norm | $FMT en-$LANG $SET $SYS Meteor
+            java -Xmx700M -jar $METEOR $HYP $PLAIN/references/$SET-ref.$LANG -l $LANG -norm -t universal | $FMT en-$LANG $SET $SYS Meteor
         done
     done
 done
